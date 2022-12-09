c0cb26e96cc4cd15b3f4c6307b664c2ca3446e3a
Tommy <tommy.mu@epitech.eu>
2022-12-09 14:19:17 +0100

    ✏️

--- interface/server-data/User.ts
+++ interface/server-data/User.ts
@@ -1,6 +1,22 @@
-interface IUser {
+import { model, Schema } from "mongoose";
+const uniqueValidator = require("mongoose-unique-validator");
+export interface IUser {
     _id: Number
     name: String
+    email: String
+    status: String
     password: Number
-    is_hospital: boolean
+    phoneNumber: String
 }
+
+const UserSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    status: { type: String, required: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true, unique: true },
+});
+
+UserSchema.plugin(uniqueValidator);
+
+export const UserModel = model("UserModel", UserSchema);
